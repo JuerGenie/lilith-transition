e2d9ab1c048c1343a7089b202f6b4d7b9b523291
Juer Genie Whang <juergenie@qq.com>
2021-06-26 05:52:26 +0800

    component `lilith-carousel`: dynamic render child.

--- src/components/carousel/carousel.svelte
+++ src/components/carousel/carousel.svelte
@@ -2,19 +2,28 @@
 
 <script lang="ts">
   import { onDestroy, onMount, createEventDispatcher } from "svelte";
+  import { get_current_component } from "svelte/internal";
 
   export let current_index: number = 0;
   export let width: string = "200px";
   export let height: string = "200px";
   export let stop_when_hover: boolean = false;
 
-  let content_element: HTMLElement[];
+  const self_component = get_current_component() as HTMLElement;
+  let content_element: Array<{ div: HTMLDivElement; source: HTMLElement }> = [];
 
-  let slot: HTMLSlotElement;
   export function update_content() {
-    content_element = slot.assignedElements() as HTMLElement[];
-    clearInterval(timer);
+    content_element = [];
+    const children = self_component.children;
+    for (let i = 0; i < children.length; i++) {
+      content_element.push({
+        div: null as unknown as HTMLDivElement,
+        source: children.item(i) as HTMLElement,
+      });
+    }
+    content_element.forEach((el, i) => (el.source.slot = String(i)));
 
+    clearInterval(timer);
     timer = setInterval(() => {
       if (canTrans) {
         current_index += 1;
@@ -27,18 +36,16 @@
   }
 
   $: {
-    if (slot) {
-      const target = content_element[current_index];
-      if (target) {
-        content_element.forEach((el) => {
-          if (el.style.zIndex > target.style.zIndex) {
-            el.style.zIndex = String(Number(el.style.zIndex) - 1);
-          }
-          el.classList.remove("top");
-        });
-        target.style.zIndex = "3";
-        target.classList.add("top");
-      }
+    const target = content_element[current_index];
+    if (target && target.div) {
+      content_element.forEach((el) => {
+        if (el.div.style.zIndex > target.div.style.zIndex) {
+          el.div.style.zIndex = String(Number(el.div.style.zIndex) - 1);
+        }
+        el.div.classList.remove("top");
+      });
+      target.div.style.zIndex = "3";
+      target.div.classList.add("top");
     }
   }
 
@@ -53,42 +60,6 @@
   });
 </script>
 
-<svelte:head>
-  <style>
-    :root {
-      --lilith-transition-density: 20px;
-    }
-    lilith-carousel::part(content) {
-      overflow: hidden;
-    }
-    lilith-carousel > * {
-      position: absolute;
-
-      top: 0;
-      left: 0;
-      right: 0;
-      bottom: 0;
-      margin: auto;
-
-      mask-image: paint(lilith-transition);
-      -webkit-mask-image: paint(lilith-transition);
-
-      --lilith-transition-position: 100%;
-    }
-    lilith-carousel > .top {
-      animation: lilith-carousel-animation 2s linear;
-    }
-    @keyframes lilith-carousel-animation {
-      0% {
-        --lilith-transition-position: -100%;
-      }
-      100% {
-        --lilith-transition-position: 100%;
-      }
-    }
-  </style>
-</svelte:head>
-
 <template>
   <div
     style={`width: ${width}; height: ${height}; position: relative;`}
@@ -96,15 +67,50 @@
     on:mouseenter={() => stop_when_hover && (canTrans = false)}
     on:mouseleave={() => stop_when_hover && (canTrans = true)}
   >
-    <slot bind:this={slot} />
+    {#each content_element as _, index}
+      <div
+        part={`wrapper wrapper-${index}`}
+        class={["wrapper", index === current_index ? "top" : ""].join(" ")}
+        bind:this={content_element[index].div}
+      >
+        <slot name={index} />
+      </div>
+    {/each}
   </div>
 </template>
 
 <style lang="scss">
-  [part="content"] {
-    transition: box-shadow 100ms;
-    // &:hover {
-    //   box-shadow: 0 4px 16px 0 #000;
-    // }
+  :root {
+    --lilith-transition-density: 20px;
+  }
+
+  :host(::part(content)) {
+    overflow: hidden;
+  }
+
+  .wrapper {
+    position: absolute;
+
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    margin: auto;
+
+    mask-image: paint(lilith-transition);
+    -webkit-mask-image: paint(lilith-transition);
+
+    --lilith-transition-position: 100%;
+  }
+  .top {
+    animation: lilith-carousel-animation 2s linear;
+  }
+  @keyframes lilith-carousel-animation {
+    0% {
+      --lilith-transition-position: -100%;
+    }
+    100% {
+      --lilith-transition-position: 100%;
+    }
   }
 </style>
